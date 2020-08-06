d2793e142a04b0726084799ad5867e2bcdd731c7
Chris Corbin <cjcorbin@gmail.com>
2020-08-06 14:42:14 -0700

    MVP+

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,14 +1,36 @@
+import axios from "axios";
+
+const card = document.querySelector(".cards");
+const myUserData = "https://api.github.com/users/chrisjcorbin";
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
 
+axios
+  .get("https://api.github.com/users/chrisjcorbin")
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log("This appears to be broken..." + error);
+  });
+
+axios
+  .get(myUserData)
+  .then((response) => {
+    card.prepend(cardMaker(response));
+  })
+  .catch((error) => {
+    console.log("This appears to be broken still..." + error);
+  });
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
-
     Skip to STEP 3.
 */
 
@@ -23,17 +45,32 @@
     manually find some other users' github handles, or use the list found at the
     bottom of the page. Get at least 5 different Github usernames and add them as
     Individual strings to the friendsArray below.
-
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell",
+];
+
+followersArray.forEach((item) => {
+  axios
+    .get("https://api.github.com/users/" + item)
+    .then((response) => {
+      card.appendChild(cardMaker(response));
+    })
+    .catch((error) => {
+      console.log("This appears to be broken again..." + error);
+    });
+});
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
@@ -46,9 +83,82 @@ const followersArray = [];
         <p>Followers: {users followers count}</p>
         <p>Following: {users following count}</p>
         <p>Bio: {users bio}</p>
+        <button>More Info</button> 
+        <div class="more-info closed">
+            <p class="contact"> Contact: {users email}</p> 
+            <p>Repos: 
+            <a href = {address to repos page}>{address to users github repos page}</a> 
+            </p>
+        </div>
       </div>
     </div>
 */
+
+function cardMaker(user) {
+  const card = document.createElement("div");
+  const userImg = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const userName = document.createElement("h3");
+  const userHandle = document.createElement("p");
+  const userLocale = document.createElement("p");
+  const userProfile = document.createElement("p");
+  const profileLink = document.createElement("a");
+  const userFollowers = document.createElement("p");
+  const userFollows = document.createElement("p");
+  const userBio = document.createElement("p");
+  const button = document.createElement("button");
+  const contactCard = document.createElement("div");
+  const userContact = document.createElement("p");
+  const userRepos = document.createElement("p");
+  const reposLink = document.createElement("a");
+  const twitterHandle = document.createElement("p");
+
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  userName.classList.add("name");
+  userHandle.classList.add("username");
+  button.classList.add("button");
+  contactCard.classList.add("closed");
+  userContact.classList.add("contact");
+
+  card.appendChild(userImg);
+  card.appendChild(cardInfo);
+  userProfile.textContent = "Profile: ";
+  userProfile.appendChild(profileLink);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(userHandle);
+  cardInfo.appendChild(userLocale);
+  cardInfo.appendChild(userProfile);
+  cardInfo.appendChild(userFollowers);
+  cardInfo.appendChild(userFollows);
+  cardInfo.appendChild(userBio);
+  cardInfo.appendChild(button);
+  cardInfo.appendChild(contactCard);
+  contactCard.appendChild(userContact);
+  contactCard.appendChild(userRepos);
+  userRepos.textContent = "User's repos: ";
+  userRepos.appendChild(reposLink);
+  twitterHandle.textContent = "Twitter: " + user.data.twitter_username;
+  contactCard.appendChild(twitterHandle);
+  userImg.src = user.data.avatar_url;
+  userName.textContent = user.data.name;
+  userHandle.textContent = user.data.login;
+  userLocale.textContent = user.data.location;
+  profileLink.textContent = user.data.html_url;
+  profileLink.setAttribute("href", user.data.html_url);
+  userFollowers.textContent = "Followers: " + user.data.followers;
+  userFollows.textContent = "Following: " + user.data.following;
+  userBio.textContent = "Bio: " + user.data.bio;
+  button.textContent = "More Info";
+  userContact.textContent = "Email: " + user.data.email;
+  reposLink.textContent = "Repos Link";
+  reposLink.setAttribute("href", user.data.repos_url);
+
+  button.addEventListener("click", () => {
+    contactCard.classList.toggle("closed");
+  });
+  return card;
+}
 
 /*
   List of LS Instructors Github username's:
